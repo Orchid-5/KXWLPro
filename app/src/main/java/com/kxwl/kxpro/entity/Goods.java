b788dfa93bc4f71fba5a8be30f66f4fcf4f18284
cp <467937566@qq.com>
2017-02-20 12:00:29 +0800

    add goods entity

---
+++ app/src/main/java/com/kxwl/kxpro/entity/Goods.java
@@ -0,0 +1,12 @@
+package com.kxwl.kxpro.entity;
+
+/**
+ * Created by answer on 2017-02-20.
+ */
+public class Goods {
+    private String name;
+    private String barCode;
+    private String specification;
+    private String brand;
+    private String source;
+}
